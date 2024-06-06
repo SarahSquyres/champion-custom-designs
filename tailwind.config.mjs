5b358b1b13c3536618ee784e59ae904acce6f4c9
Sarah Squyres <ssquyres15@gmail.com>
2024-06-05 23:46:34 -0500

    added carousel auto scroll

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'infinite-scroll': 'infinite-scroll 100s linear infinite',
+			  },
+			  keyframes: {
+				'infinite-scroll': {
+
+				  '0': {
+					transform: 'translate3d(0, 0, 0)',
+				  },
+				  '100%': {
+					transform: 'translate3d(-300%, 0, 0)',
+				  },
+				},
+			  },
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('daisyui'),
+	],
 }
