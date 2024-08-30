aff2c2007942df6470548fc976792f84b45ec15e
unknown <ssquyres15@gmail.com>
2024-08-29 22:28:16 -0500

    image slideshow and begin footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import NavBar from '../src/components/NavBar'
+import NavBar from '../src/components/NavBar';
+import Slideshow from '../src/components/Slideshow';
+import Footer from './components/Footer';
 
 function App () {
   
   return(
     <>
     <NavBar />
+    <Slideshow />
+    <Footer />
     </>
 
   );
